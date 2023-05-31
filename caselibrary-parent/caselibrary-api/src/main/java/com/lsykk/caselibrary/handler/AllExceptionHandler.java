8bc9c264f1f7adddac2568862cd803d1608bed41
lsy-kk <1275917295@qq.com>
2023-05-31 13:53:35 +0800

    feature(handler): 异常处理器调整

--- caselibrary-parent/caselibrary-api/src/main/java/com/lsykk/caselibrary/handler/AllExceptionHandler.java
+++ caselibrary-parent/caselibrary-api/src/main/java/com/lsykk/caselibrary/handler/AllExceptionHandler.java
@@ -3,6 +3,7 @@ package com.lsykk.caselibrary.handler;
 import com.lsykk.caselibrary.vo.ApiResult;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.ResponseBody;
 
 // 对加了@Controller注解的类，进行异常的拦截处理 AOP的实现
 @ControllerAdvice
@@ -10,6 +11,7 @@ public class AllExceptionHandler {
 
     // 处理Exception.class
     @ExceptionHandler
+    @ResponseBody
     public ApiResult doException(Exception exception){
         exception.printStackTrace();
         return ApiResult.fail("?", "系统异常");
